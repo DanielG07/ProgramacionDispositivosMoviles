89bfcbe8f18d9836d35fcce7896aed38d46ac2f8
Daniel González Jiménez <danielgj7@outlook.com>
2022-10-23 19:09:45 -0500

    Se agrega funcionalidad

--- Practica9/app/src/main/java/com/danielg7/practica9/MainActivity.kt
+++ Practica9/app/src/main/java/com/danielg7/practica9/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.danielg7.practica9
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvArchivo: TextView = findViewById(R.id.tvArchivo)
+
+        val entradaDatos: InputStream = resources.openRawResource(R.raw.texto)
+        val stream = InputStreamReader(entradaDatos)
+        val buffer = BufferedReader(stream)
+
+        var lineaDatos: String?
+        while (true) {
+            try {
+                lineaDatos = buffer.readLine()
+                if (lineaDatos == null) {
+                    break
+                }
+                println(lineaDatos)
+                tvArchivo.append("""
+                    
+                    $lineaDatos
+                    
+                """.trimIndent())
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
